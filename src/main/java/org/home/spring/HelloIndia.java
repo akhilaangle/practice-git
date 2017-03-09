9be343f7bf8722ea9a08cf34135fc582cd75fe07
Akhila Mupparaju <Akhila Mupparaju>
2017-03-09 00:30:50 -0500

    first commit to practice-git

---
+++ src/main/java/org/home/spring/HelloIndia.java
@@ -0,0 +1,10 @@
+package org.home.spring;
+
+/**
+ * Created by Akhila on 3/9/2017.
+ */
+public class HelloIndia {
+    public static void main(String[] args) {
+        System.out.println("Hello Akhila Bangaram");
+    }
+}
